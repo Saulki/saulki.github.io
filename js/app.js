6c1d924c58a7ae7982c03f52246b8ec4d7d7eea8
Saulius Kirklys <saulius.kirklys@gmail.com>
2015-03-29 19:32:20 +0300

    Works

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+
+var mq = window.matchMedia( "only screen and (min-width:40.063em)" );
+if (mq.matches) {
+	$(window).scroll(function() {
+	    if ($(document).scrollTop() > 100) {
+	        $(".contain-to-grid").addClass('smaller');
+	    }
+	    else {
+	        $(".contain-to-grid").removeClass('smaller');
+	    }
+	});
+};
